051b00631d44f67e272b966ed0294bb566bf2463
ChatNoir76 <julien.riviere47@orange.fr>
2019-03-27 09:54:43 +0100

    Prise en charge d'une erreur source de type WReaderException

--- InterfaceModeOp2/Controller/WActionException.vb
+++ InterfaceModeOp2/Controller/WActionException.vb
@@ -1,6 +1,8 @@
 ﻿Public Class WActionException
     Inherits Exception
 
+    Private Const _NOSOURCE = "pas d'erreur source"
+    Private Const _SOURCEWREADER = "WReader est la source de l'erreur {0} ayant comme cause {1}"
     Private _ErreurSource As String
 
     Public ReadOnly Property getErreurSource As String
@@ -9,15 +11,33 @@
         End Get
     End Property
 
+    Sub New(ByVal errmsg As String)
+        MyBase.New(errmsg)
+        _ErreurSource = _NOSOURCE
+    End Sub
+
     ''' <summary>
     ''' Permet l'identification précise d'une erreur généré par WAction
     ''' </summary>
     ''' <param name="ex">Exception d'origine</param>
     ''' <param name="errmsg">Message d'erreur</param>
     ''' <remarks></remarks>
-    Sub New(ByVal errmsg As String, Optional ByVal ex As String = "Pas d'erreur source")
+    Sub New(ByVal errmsg As String, ByVal ex As Exception)
         MyBase.New(errmsg)
-        _ErreurSource = ex
+        If IsNothing(ex) Then
+            _ErreurSource = _NOSOURCE
+        Else
+            _ErreurSource = ex.Message
+        End If
+    End Sub
+
+    Sub New(ByVal errmsg As String, ByVal ex As WReaderException)
+        MyBase.New(errmsg)
+        If IsNothing(ex) Then
+            _ErreurSource = _NOSOURCE
+        Else
+            _ErreurSource = String.Format(_SOURCEWREADER, ex.Message, ex.getErreurSource)
+        End If
     End Sub
 
 
